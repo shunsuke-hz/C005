f4611411615117daf15d3335a858b4d8c0b4b13c
shunsuke-hz <jirudore@gmail.com>
2020-03-24 19:56:46 +0900

    update

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+// Your code here!
+// // 標準入力から値を取得してinput_lineに入れる
+$input_line = fgets(STDIN);
+$ipAddresses = [];
+$judge = '';
+
+while ($input = fgets(STDIN)) {
+    $ipAddresses[] = trim($input);
+}
+
+foreach ($ipAddresses as $value) {
+    $ipAddress = explode(".", $value);
+    if (count($ipAddress) === 4) {
+        foreach ($ipAddress as $ip) {
+            if ($ip < 0 || $ip > 255) {
+                $judge = 'False' . "\n";
+                echo $judge;
+                continue 2;
+            }
+        }
+        $judge = 'True' . "\n";
+    } else {
+        $judge = 'False' . "\n";
+    }
+    echo $judge;
+}
+
+?>
